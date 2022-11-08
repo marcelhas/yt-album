12ecb5f5ab879114e7a1f03fe7019b779c1fcf76
marcelhas <marcel.hasieber@gmail.com>
2022-11-08 22:42:48 +0100

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  # See <https://github.com/pimeys/nix-prisma-example>.
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          yt-dlp-light
+          ffmpeg
+        ];
+      };
+    });
+}
