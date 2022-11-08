ec403dd525bbab16fd9d950d485c8fafea7aecba
marcelhas <marcel.hasieber@gmail.com>
2022-11-08 15:03:31 +0100

    Add youtube download

--- ytdl-album.sh
+++ ytdl-album.sh
@@ -20,6 +20,21 @@ if [[ -z "${1-}" || "${1-}" =~ ^-*h(elp)?$ ]]; then
     exit
 fi
 
+URL="$1"
+TMP="$(mktemp -d)"
+ALBUM="$TMP/album.mp3"
+# TODO: Use script directory instead of hardcoding.
+OUT="./out"
+
+rm -rf "$OUT"
+mkdir -p "$OUT"
+
+yt-dlp -x --split-chapters --audio-quality 0 --audio-format mp3 \
+       --quiet --progress --console-title --newline --progress-template "postprocess:[Processing: %(info.title)s ...]" \
+       --windows-filenames --restrict-filenames                 \
+       -o "$ALBUM" -o "chapter:$OUT/%(title)s_%(section_number)03d_%(section_title)s%(ext)s" \
+       "$URL"
+
 while read -r start end out; do
     echo "$start - $end - $out"
     ffmpeg -nostdin -y -ss "$start" -to "$end" -i album.mp3 "out/$out.mp3"
