92ceffc1f1b1929d00bbd147141f596de00b97bf
marcelhas <marcel.hasieber@gmail.com>
2022-11-08 11:32:02 +0100

    Add bash boilerplate

--- ytdl-album.sh
+++ ytdl-album.sh
@@ -1,4 +1,24 @@
-#!/usr/env/bash
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Allows running in debug mode by setting the TRACE environment variable.
+# e.g. <TRACE=1 ./ytdl-album.sh>
+if [[ "${TRACE-0}" == "1" ]]; then set -o xtrace; fi
+
+usage() {
+    printf \
+    'Usage: ./ytdl-album URL
+
+Example:
+./ytdl-album https://www.youtube.com/watch?v=lmvUFhjZdFc
+'
+}
+
+if [[ -z "${1-}" || "${1-}" =~ ^-*h(elp)?$ ]]; then
+    usage
+    exit
+fi
 
 while read -r start end out; do
     echo "$start - $end - $out"
