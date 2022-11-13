e2cd7cd3ef636d94db49144f1047ef55347ebaf9
marcelhas <marcel.hasieber@gmail.com>
2022-11-13 17:13:45 +0100

    Rework flake.nix

--- flake.nix
+++ flake.nix
@@ -1,18 +1,48 @@
+# Thanks to <https://ertt.ca/nix/shell-scripts/> for his simple introduction!
 {
-  # See <https://github.com/pimeys/nix-prisma-example>.
+  description = "Download an album from Youtube and split it into sections.";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShell = pkgs.mkShell {
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        mainScriptName = "yt-album";
+        testScriptName = "test";
+        buildInputs = with pkgs; [ yt-dlp-light ffmpeg ];
+        mainScript = (pkgs.writeScriptBin mainScriptName (builtins.readFile ./yt-album.sh)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+        testScript = (pkgs.writeScriptBin testScriptName (builtins.readFile ./test.sh)).overrideAttrs(old: {
+          buildCommand = "${old.buildCommand}\n patchShebangs $out";
+        });
+
+      in rec {
+        defaultPackage = packages.yt-album;
+        # Main script.
+        packages.yt-album = pkgs.symlinkJoin {
+          name = mainScriptName;
+          paths = [ mainScript ] ++ buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${mainScriptName} --prefix PATH : $out/bin";
+        };
+        # Tests.
+        packages.test = pkgs.symlinkJoin {
+          name = testScriptName;
+          paths = [ testScript ] ++ buildInputs;
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = "wrapProgram $out/bin/${testScriptName} --prefix PATH : $out/bin";
+        };
+        # Dev environment.
+        devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [
           yt-dlp-light
           ffmpeg
         ];
       };
-    });
+      }
+    );
 }
