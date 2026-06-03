6609af9bd382de6a6906257ada6f506c76280efb
marcelhas <marcel.hasieber@gmail.com>
2026-06-03 22:26:56 +0200

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -5,48 +5,65 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         mainScriptName = "yt-album";
         testScriptName = "test";
-        buildInputs = with pkgs; [ deno ffmpeg yt-dlp-light ];
-        mainScript = (pkgs.writeScriptBin mainScriptName
-          (builtins.readFile ./yt-album.sh)).overrideAttrs (old: {
-            buildCommand = ''
-              ${old.buildCommand}
-               patchShebangs $out'';
-          });
-        testScript = (pkgs.writeScriptBin testScriptName
-          (builtins.readFile ./test.sh)).overrideAttrs (old: {
-            buildCommand = ''
-              ${old.buildCommand}
-               patchShebangs $out'';
-          });
+        buildInputs = with pkgs; [
+          deno
+          ffmpeg
+          yt-dlp-light
+        ];
+        mainScript =
+          (pkgs.writeScriptBin mainScriptName (builtins.readFile ./yt-album.sh)).overrideAttrs
+            (old: {
+              buildCommand = ''
+                ${old.buildCommand}
+                 patchShebangs $out'';
+            });
+        testScript =
+          (pkgs.writeScriptBin testScriptName (builtins.readFile ./test.sh)).overrideAttrs
+            (old: {
+              buildCommand = ''
+                ${old.buildCommand}
+                 patchShebangs $out'';
+            });
 
-      in rec {
+      in
+      rec {
         defaultPackage = packages.yt-album;
         # Main script.
         packages.yt-album = pkgs.symlinkJoin {
           name = mainScriptName;
           paths = [ mainScript ] ++ buildInputs;
           buildInputs = [ pkgs.makeWrapper ];
-          postBuild =
-            "wrapProgram $out/bin/${mainScriptName} --prefix PATH : $out/bin";
+          postBuild = "wrapProgram $out/bin/${mainScriptName} --prefix PATH : $out/bin";
         };
         # Tests.
         packages.test = pkgs.symlinkJoin {
           name = testScriptName;
           paths = [ testScript ] ++ buildInputs;
           buildInputs = [ pkgs.makeWrapper ];
-          postBuild =
-            "wrapProgram $out/bin/${testScriptName} --prefix PATH : $out/bin";
+          postBuild = "wrapProgram $out/bin/${testScriptName} --prefix PATH : $out/bin";
         };
         # Dev environment.
         devShell = pkgs.mkShell {
           nativeBuildInputs = [ pkgs.bashInteractive ];
-          buildInputs = with pkgs; [ deno ffmpeg nixfmt yt-dlp-light ];
+          buildInputs = with pkgs; [
+            deno
+            ffmpeg
+            nixfmt
+            yt-dlp-light
+          ];
         };
-      });
+      }
+    );
 }
